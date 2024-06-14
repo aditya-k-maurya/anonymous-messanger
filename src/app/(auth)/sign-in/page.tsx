42ee483e7f9c3459acb65c96da31960df9142369
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-06-14 22:47:37 +0530

    shadcn ui setup

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,22 +1,15 @@
 "use client";
 
-import { useSession, signIn, signOut } from "next-auth/react";
-export default function Component() {
-	const { data: session } = useSession();
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import * as z from "zod"
+import Link from "next/link"
 
-	if (session) {
-		return (
-			<>
-				Signed in as {session.user.email} <br />
-				<button onClick={() => signOut()}>Sign out</button>
-			</>
-		);
-	}
 
+const page = () => {
 	return (
-		<>
-			Not Signed in <br />
-			<button className="bg-orange-500 px-3 py-1 m-4 rounded" onClick={() => signIn()}>Sign in</button>
-		</>
-	);
+		<div>page</div>
+	)
 }
+
+export default page
