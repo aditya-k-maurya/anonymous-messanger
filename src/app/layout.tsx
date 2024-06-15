5fa443713fe7802c1040d3b6560cc02473f725d0
Aditya-Kumar-Maurya <aditya.maurya.1003@gmail.com>
2024-06-15 18:07:14 +0530

    signup api-fetch

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import AuthProvider from '@/context/AuthProvider'
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,11 +17,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <AuthProvider>
-
-      <body className={inter.className}>{children}</body>
-      </AuthProvider>
-    </html>
-  );
+		<html lang="en">
+			<AuthProvider>
+				<body className={inter.className}>
+					{children}
+					<Toaster />
+				</body>
+			</AuthProvider>
+		</html>
+	);
 }
